9dc77b9c20ccb9849609ec07d2858e2a6bb876a4
Alexis DEGRYSE <adegryse@ineat.fr>
2020-12-29 12:02:42 +0100

    Modals

--- src/js/components/modals.js
+++ src/js/components/modals.js
@@ -4,43 +4,53 @@ let previouslyFocusedElement = null;
 
 const focusableSelector = 'button:not([disabled]), a[href], input:not([type="hidden"]):not([disabled]), textarea:not([disabled]), select:not([disabled]), [contenteditable]';
 
-const openModal = function (e, modalID) {
-	if (e) {
-		e.preventDefault();
-		modalID = e.currentTarget.getAttribute('data-modal-id');
-	}
-
-	if (modalID) {
-		modal = document.querySelector('#'+modalID);
-	} else {
-		modal = modalID;
-	}
-
-	focusables = Array.from(modal.querySelectorAll(focusableSelector));
+const openModal = function (modalID) {
+	// Keep current modal & his trigger
+	modal = document.querySelector('#'+modalID);
 	previouslyFocusedElement = document.querySelector(':focus');
+
+	// Add specific styles to <body>
 	body.classList.add('modal-is-open');
+
+	// Set attribute to <dialog> element
 	modal.setAttribute('open', '');
+
+	// Check focusable elements in the modal then give focus to the first one
+	focusables = Array.from(modal.querySelectorAll(focusableSelector));
 	focusables[0].focus();
+
+	// Init closing modal functions
 	modal.addEventListener('click', closeModal);
-	modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+	modal.querySelectorAll('.js-modal-close').forEach((btn) => {
+		btn.addEventListener('click', closeModal);
+	});
 	modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
 };
 
-const closeModal = function (e) {
+const closeModal = function () {
+	// Abort function if there is not opened modal
 	if (modal === null) {
 		return;
 	}
+
+	// Give focus to the trigger
 	if (previouslyFocusedElement !== null) {
 		previouslyFocusedElement.focus();
 	}
-	if (e) {
-		e.preventDefault();
-	}
+
+	// Add fading-out class to modal
 	modal.classList.add('is-hiding');
+
+	// Remove closing modal functions
 	modal.removeEventListener('click', closeModal);
-	modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+	modal.querySelectorAll('.js-modal-close').forEach((btn) => {
+		btn.removeEventListener('click', closeModal);
+	});
 	modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+
+	//
 	const hideModal = function () {
+		// Undo all the things
 		modal.classList.remove('is-hiding');
 		modal.removeAttribute('open');
 		body.classList.remove('modal-is-open');
@@ -48,6 +58,8 @@ const closeModal = function (e) {
 		modal = null;
 		getScrollbarWidth();
 	};
+
+	// When the modal has finished to fade out
 	modal.addEventListener('animationend', hideModal);
 };
 
@@ -56,11 +68,7 @@ const stopPropagation = function (e) {
 };
 
 document.querySelectorAll('.js-modal').forEach((a) => {
-	a.addEventListener('click', openModal);
-});
-
-window.addEventListener('keydown', (e) => {
-	if (e.key === 'Escape' || e.key === 'Esc') {
-		closeModal(e);
-	}
+	a.addEventListener('click', () => {
+		openModal(a.getAttribute('data-modal-id'));
+	});
 });
