154f4079f4902b80262bf5794512034e1e2ad9c1
Alexis DEGRYSE <adegryse@ineat.fr>
2020-12-29 16:05:40 +0100

    Dropdowns

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,27 +1,30 @@
 var demoSlider = document.querySelector('.js-slider-demo');
 
-tns({
-    container: demoSlider.querySelector('.p-slider__content'),
-    items: 1,
-    slideBy: 'page',
-    nav: 'true',
-    navPosition: 'bottom',
-    controlsContainer: demoSlider.querySelector('.p-slider__controls'),
-    arrowKeys: true, 
-    autoplayButtonOutput: false,
-    loop: false,
-    responsive: {
-        576: {
-            items: 2,
-            gutter: baseFontSize * getComputedStyle(html).getPropertyValue('--spacing-unitless')
+if(demoSlider) {
+
+    tns({
+        container: demoSlider.querySelector('.p-slider__content'),
+        items: 1,
+        slideBy: 'page',
+        nav: 'true',
+        navPosition: 'bottom',
+        controlsContainer: demoSlider.querySelector('.p-slider__controls'),
+        arrowKeys: true, 
+        autoplayButtonOutput: false,
+        loop: false,
+        responsive: {
+            576: {
+                items: 2,
+                gutter: baseFontSize * getComputedStyle(html).getPropertyValue('--spacing-unitless')
+            },
+            1200: {
+                items: 3
+            }
         },
-        1200: {
-            items: 3,
-            //edgePadding: baseFontSize * parseInt(getComputedStyle(html).getPropertyValue('--spacing-unitless'))
+        preventScrollOnTouch: 'auto',
+        onInit: function() {
+            demoSlider.style.display = 'block';
         }
-    },
-    preventScrollOnTouch: 'auto',
-    onInit: function() {
-        demoSlider.style.display = 'block';
-    }
-});
+    });
+
+}
