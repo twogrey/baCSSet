2b7140bbc7e317143a7d8da67cb09e46f91ce7e8
Alexis DEGRYSE <adegryse@ineat.fr>
2021-03-29 17:31:48 +0200

    Range slider: with unit or with negative minimum

--- src/js/components/ranges.js
+++ src/js/components/ranges.js
@@ -1,14 +1,13 @@
-function updateRangeOutput(range) {
+function updateRange(range) {
 	let container = range.closest('.js-range'),
 			nbRanges = container.querySelectorAll('[type="range"]').length,
 			value = parseFloat(range.value),
-			inputMin = null,
+			inputMin = container.querySelector('[type="range"]:first-of-type'),
 			inputMax = null,
 			outputMin = container.querySelector('.range__output'),
 			outputMax = null;
 
 	if(container.querySelectorAll('[type="range"]').length === 2) {
-		inputMin = container.querySelector('[type="range"]:first-of-type');
 		inputMax = container.querySelector('[type="range"]:last-of-type');
 		outputMin = container.querySelector('.range__output:first-of-type');
 		outputMax = container.querySelector('.range__output:last-of-type');
@@ -17,24 +16,32 @@ function updateRangeOutput(range) {
 	if(range.matches(':first-of-type')) {
 		if(inputMax && value >= parseFloat(inputMax.value)) {
 			inputMax.value = value;
-			outputMax.setAttribute('data-value', value);
-			container.style.setProperty('--b', value);
+			updateThumbText(container, inputMax, outputMax, value);
 		}
-		outputMin.setAttribute('data-value', value);
-		container.style.setProperty('--a', value);
+		updateThumbText(container, inputMin, outputMin, value);
 	} else {
 		if(value <= parseFloat(inputMin.value)) {
 			inputMin.value = value;
-			outputMin.setAttribute('data-value', value);
-			container.style.setProperty('--a', value);
+			updateThumbText(container, inputMin, outputMin, value);
 		}
-		outputMax.setAttribute('data-value', value);
+		updateThumbText(container, inputMax, outputMax, value);
+	}
+}
+
+function updateThumbText(container, input, output, value) {
+	output.setAttribute('data-value', value);
+	if(input.matches(':first-of-type')) {
+		container.style.setProperty('--a', value);
+	} else {
 		container.style.setProperty('--b', value);
+	}
+	if(input.getAttribute('data-unit')) {
+		input.setAttribute('aria-valuetext', input.value+""+input.getAttribute('data-unit'));
 	}
 }
 
 document.querySelectorAll('.js-range [type="range"]').forEach((range) => {
 	range.addEventListener('input', () => {
-		updateRangeOutput(range);
+		updateRange(range);
 	});
 });
